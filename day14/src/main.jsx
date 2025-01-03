f81be69d1c6a0fb316793bf4ec0483bf654474b7
yashsoni2191 <yashsoni2191@gmail.com>
2025-01-03 20:17:40 +0530

    form-product-validation

--- day14/src/main.jsx
+++ day14/src/main.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
